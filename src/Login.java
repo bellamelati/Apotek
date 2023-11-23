73ada4b444f6564fb7c0120d740447032a6d7006
nasyw4 <nasywaraihanah@gmail.com>
2023-11-23 15:44:57 +0700

    Membuat GUI Login

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
 public class Login {
+    public void showGUI(){
+        // Membuat frame
+        JFrame frame = new JFrame("Apotek");
+
+        // Mengatur properti frame
+        frame.setSize(640, 480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Menempatkan frame di tengah layar
+        frame.setLocationRelativeTo(null);
+
+        // Menampilkan frame
+        frame.setVisible(true);
+    }
 }
