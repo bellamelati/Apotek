7153789b03a9dd23a6c18ad729cdf98460ab2071
Bella Melati <biabellamwdf@gmail.com>
2023-12-11 18:07:37 +0700

    update obat

--- src/Obat.java
+++ src/Obat.java
@@ -1,41 +1,32 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.table.*;
-import java.util.Date;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.*;
-import java.text.SimpleDateFormat;
-//import com.toedter.calendar.JDateChooser;
-
+import java.util.Vector;
 
 public class Obat extends JFrame {
 
-    // Komponen GUI
     private JLabel tambahLabel;
     private JLabel hapusLabel;
     private JLabel ubahLabel;
     private JLabel bersihLabel;
     private JLabel cariLabel;
-    private JLabel logoLabel;
-    private JLabel titleLabel;
 
     private JLabel kodeObatLabel;
     private JTextField kodeObatField;
-
     private JLabel namaObatLabel;
     private JTextField namaObatField;
-
     private JLabel hargaLabel;
     private JTextField hargaField;
-
     private JLabel stokLabel;
     private JTextField stokField;
-
     private JLabel keteranganLabel;
     private JTextField keteranganField;
-
     private JLabel expDateLabel;
-//    private JDateChooser expDateChooser;
+    private JTextField expDateField;
 
     private JTextField cariField;
 
@@ -43,15 +34,26 @@ public class Obat extends JFrame {
     private JTable obatTable;
     private JScrollPane tableScrollPane;
 
-//    private LabelTime waktu;
+    private Connection connection;
 
     public Obat() {
         initializeFrame();
-   //     initializeComponents();
-   //     addActionListeners();
+        initializeComponents();
+        addActionListeners();
+        initializeDatabaseConnection();
+        tampilTabel();
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+    }
+
+    private void initializeDatabaseConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/apkapotek", "root", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initializeFrame() {
@@ -61,20 +63,180 @@ public class Obat extends JFrame {
         setTitle("Database Obat");
     }
 
-    private class AddObatListener extends FeatureListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            openObatGUI();
-        }
+    private void initializeComponents() {
+        tambahLabel = new JLabel("Tambah");
+        hapusLabel = new JLabel("Hapus");
+        ubahLabel = new JLabel("Ubah");
+        bersihLabel = new JLabel("Bersih");
+        cariLabel = new JLabel("Cari");
 
-        private void openObatGUI() {
-            new Obat();
+        kodeObatLabel = new JLabel("Kode Obat");
+        kodeObatField = new JTextField();
+        namaObatLabel = new JLabel("Nama Obat");
+        namaObatField = new JTextField();
+        hargaLabel = new JLabel("Harga");
+        hargaField = new JTextField();
+        stokLabel = new JLabel("Stok");
+        stokField = new JTextField();
+        keteranganLabel = new JLabel("Keterangan");
+        keteranganField = new JTextField();
+        expDateLabel = new JLabel("Exp_Date");
+        expDateField = new JTextField();
+
+        cariField = new JTextField();
+
+        // Initialize the table and table model
+        String[] columnNames = {"Kode Obat", "Nama Obat", "Harga", "Stok", "Keterangan", "Exp_Date"};
+        tableModel = new DefaultTableModel(columnNames, 0);
+        obatTable = new JTable(tableModel);
+        tableScrollPane = new JScrollPane(obatTable);
+
+        setLayout(new BorderLayout());
+        JPanel inputPanel = new JPanel(new GridLayout(7, 2));
+
+        inputPanel.add(kodeObatLabel);
+        inputPanel.add(kodeObatField);
+        inputPanel.add(namaObatLabel);
+        inputPanel.add(namaObatField);
+        inputPanel.add(hargaLabel);
+        inputPanel.add(hargaField);
+        inputPanel.add(stokLabel);
+        inputPanel.add(stokField);
+        inputPanel.add(keteranganLabel);
+        inputPanel.add(keteranganField);
+        inputPanel.add(expDateLabel);
+        inputPanel.add(expDateField);
+
+        add(inputPanel, BorderLayout.NORTH);
+        add(tableScrollPane, BorderLayout.CENTER);
+    }
+
+    private void addActionListeners() {
+        tambahLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                openObatGUI();
+            }
+
+            private void openObatGUI() {
+                new Obat();
+            }
+        });
+
+        hapusLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int selectedRow = obatTable.getSelectedRow();
+                if (selectedRow != -1) {
+                    int confirmDialog = JOptionPane.showConfirmDialog(
+                            null,
+                            "Apakah Anda yakin ingin menghapus data ini?",
+                            "Konfirmasi Hapus Data",
+                            JOptionPane.YES_NO_OPTION);
+
+                    if (confirmDialog == JOptionPane.YES_OPTION) {
+                        hapusData(selectedRow);
+                        tampilTabel();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Pilih baris yang akan dihapus terlebih dahulu.",
+                            "Peringatan",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+
+        ubahLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int selectedRow = obatTable.getSelectedRow();
+                if (selectedRow != -1) {
+                    Vector<Object> rowData = new Vector<>();
+                    for (int i = 0; i < tableModel.getColumnCount(); i++) {
+                        rowData.add(obatTable.getValueAt(selectedRow, i));
+                    }
+                    openUbahObatGUI(rowData);
+                } else {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Pilih baris yang akan diubah terlebih dahulu.",
+                            "Peringatan",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+
+            private void openUbahObatGUI(Vector<Object> rowData) {
+                // Implementasi untuk membuka jendela ubah data dengan data terpilih
+                // Gunakan data dari rowData untuk mengisi form pada jendela ubah data
+                // Anda dapat membuat kelas baru untuk jendela ubah data atau
+                // menambahkan logika ubah data di sini.
+            }
+        });
+
+        cariLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String searchText = cariField.getText().trim();
+                TableRowSorter<DefaultTableModel> sorter =
+                        new TableRowSorter<>((DefaultTableModel) obatTable.getModel());
+                obatTable.setRowSorter(sorter);
+
+                if (searchText.length() == 0) {
+                    sorter.setRowFilter(null);
+                } else {
+                    try {
+                        sorter.setRowFilter(RowFilter.regexFilter("(?i)" + searchText));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void tampilTabel() {
+        try {
+            DefaultTableModel model = (DefaultTableModel) obatTable.getModel();
+            model.setRowCount(0);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM db_obat");
+
+            while (resultSet.next()) {
+                Vector<Object> rowData = new Vector<>();
+                rowData.add(resultSet.getString("ID_Obat"));
+                rowData.add(resultSet.getString("NamaObat"));
+                rowData.add(resultSet.getString("Harga"));
+                rowData.add(resultSet.getString("Stok"));
+                rowData.add(resultSet.getString("Keterangan"));
+                rowData.add(resultSet.getString("Exp_Date"));
+
+                model.addRow(rowData);
+            }
+
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    private abstract class FeatureListener implements ActionListener {
-        @Override
-        public abstract void actionPerformed(ActionEvent e);
+    private void hapusData(int selectedRow) {
+        try {
+            DefaultTableModel model = (DefaultTableModel) obatTable.getModel();
+            String kodeObat = model.getValueAt(selectedRow, 0).toString();
+
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM db_obat WHERE ID_Obat = ?");
+            preparedStatement.setString(1, kodeObat);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    public static void main(String[] args) {
+        new Obat();
+    }
 }
