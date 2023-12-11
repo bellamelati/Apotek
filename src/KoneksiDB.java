f64d4962a5f39cfb79c8784045c84e12cd945533
DESKTOP-AT0K5A9\User <mohamadjulfanra29@gmail.com>
2023-12-11 20:33:44 +0700

    penambahan image, ubah koneksidb

--- src/KoneksiDB.java
+++ src/KoneksiDB.java
@@ -1,28 +1,13 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import javax.swing.JOptionPane;
 
 public class KoneksiDB {
-    private static Connection koneksi;
+    private static final String URL = "jdbc:mysql://localhost/aplikasiapotek";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
-    public static Connection getKoneksi() {
-        try {
-            String url = "jdbc:mysql://localhost:3306/aplikasiapotek";
-            String user = "root";
-            String password = "";
-
-            if (koneksi == null) {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-                koneksi = DriverManager.getConnection(url, user, password);
-                JOptionPane.showMessageDialog(null, "Koneksi Berhasil !!!", "Report Koneksi", JOptionPane.INFORMATION_MESSAGE);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Gagal terhubung ke database", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-        return koneksi;
+    public static Connection getKoneksi() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
